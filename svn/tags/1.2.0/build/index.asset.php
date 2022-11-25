977f6ae34795c86a3566d7b2e8b76437fcbfbbd1
zwwuu <98434813+zwwuu@users.noreply.github.com>
2022-11-25 16:51:54 -0500

    update freemius SDK

---
+++ svn/tags/1.2.0/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '337a170b410ae367ad56');
